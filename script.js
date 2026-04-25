734a04f9e760096d4d196a5d817dbd4908eeb3a1
brunvilkerby0-commits <brunvilkerby0@gmail.com>
2026-04-24 22:27:37 -0400

    script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("btc").innerText =
+"$ " + (60000 + Math.random()*2000).toFixed(2);
+
+document.getElementById("eth").innerText =
+"$ " + (3000 + Math.random()*200).toFixed(2);
